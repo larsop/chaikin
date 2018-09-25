883409c81bd51faac6f19b599a0cec65d9302633
Lars Aksel Opsahl <lop@skogoglandskap.no>
2018-09-25 06:47:16 +0200

    not any faster maybe because we can not use IMMUTABLE ?

--- src/chaikinSmoothLineComplex.sql
+++ src/chaikinSmoothLineComplex.sql
@@ -7,9 +7,6 @@
 DROP FUNCTION IF EXISTS chaikinSmoothLineComplex(_input_line geometry,int,int,numeric,int );
 DROP FUNCTION IF EXISTS chaikinSmoothLineComplex(_input_line geometry(LineString),int,int,numeric,int );
 
-DROP TYPE chaikinSmoothLineComplex_dumpType;
-CREATE TYPE chaikinSmoothLineComplex_dumpType AS (path int,geom geometry);
-
 CREATE OR REPLACE FUNCTION chaikinSmoothLineComplex(
 _input_line geometry(LineString),
 _min_degrees int default 90,
@@ -26,16 +23,14 @@ num_points int;
 counter int = 0;
 simplfied_line geometry(LineString);
 
-simplfied_line_dump chaikinSmoothLineComplex_dumpType ARRAY;
-
 BEGIN
 
 IF (ST_GeometryType(_input_line) != 'ST_LineString' ) THEN
 	RAISE EXCEPTION 'Invalid GeometryType(_input_line) %', ST_input_lineetryType(_input_line);
 END IF;
- 
-simplfied_line := _input_line;
 
+ simplfied_line = _input_line;
+ 
 
 FOR counter IN 1.._nIterations LOOP
 
@@ -44,30 +39,30 @@ FOR counter IN 1.._nIterations LOOP
 	RAISE EXCEPTION 'To many Iterations %', _nIterations;
  END IF;
 
+   DROP table if exists rdb_temp_table ;
 
- select (dp).path,(dp).geom  into simplfied_line_dump
+CREATE temp table rdb_temp_table AS ( SELECT (dp).path[1] As org_index, lead((dp).geom) OVER () AS lead_p, (dp).geom As p1,  lag((dp).geom) OVER () AS lag_p
  FROM (
   SELECT ST_DumpPoints(simplfied_line) as dp
- ) as r;
- 
+ ) as db
+ );
+  
+      
  select array_agg(org_index) into need_to_fix_index from (
   select abs(degrees(azimuth_1-azimuth_2)) as angle,org_index
   --select 100 as angle, 1 as org_index
   from (
    SELECT org_index, 
-   ST_Azimuth(p, lead_p) as azimuth_2, 
-   ST_Azimuth(p, lag_p) as azimuth_1
-   FROM (
-    SELECT (dp).path[1] As org_index, lead((dp).geom) OVER () AS lead_p, (dp).geom As p,  lag((dp).geom) OVER () AS lag_p
-    FROM (
-     SELECT ST_DumpPoints(simplfied_line) as dp
-    ) as r
-   ) as r where not ST_Equals(lead_p,p) and not ST_Equals(p,lag_p) and
-   ST_distance(lead_p, p) < _max_distance and ST_distance(p, lag_p) < _max_distance
+   ST_Azimuth(p1, lead_p) as azimuth_2, 
+   ST_Azimuth(p1, lag_p) as azimuth_1
+   FROM 
+   rdb_temp_table
+   where not ST_Equals(lead_p,p1) and not ST_Equals(p1,lag_p) and
+   ST_distance(lead_p, p1) < _max_distance and ST_distance(p1, lag_p) < _max_distance
   ) as r where azimuth_1 is not null and azimuth_2 is not null 
  ) as r where angle <= _min_degrees or angle >= _max_degrees;
 
- --RAISE NOTICE 'need_to_fix_index aaa %', need_to_fix_index;
+ RAISE NOTICE 'need_to_fix_index aaa %', need_to_fix_index;
  
  -- if there are no sharp angles use return input as it is
  IF need_to_fix_index IS NULL THEN
@@ -81,7 +76,7 @@ FOR counter IN 1.._nIterations LOOP
  simplfied_line := ST_LineFromMultiPoint(mp) FROM (
   SELECT ST_Collect(mp) as mp FROM (
   -- how to avoid equal points ??
-    SELECT unnest(ARRAY[p1,p1_n,p2_n,p2]) as mp FROM (
+    SELECT unnest(ARRAY[p1,p1_n,p2_n,lead_p]) as mp FROM (
      SELECT 
      CASE WHEN org_index=1 THEN 
       p1
@@ -93,12 +88,12 @@ FOR counter IN 1.._nIterations LOOP
      END as p1_n,
      CASE WHEN p2_n IS NOT NULL THEN 
       p2_n
-      ELSE NULLIF(p2, p2_n)
+      ELSE NULLIF(lead_p, p2_n)
      END as p2_n,
      CASE WHEN org_index=num_points THEN 
-      p2
+      lead_p
       ELSE null
-     END as p2
+     END as lead_p
      FROM (
       SELECT 
       org_index,
@@ -111,11 +106,11 @@ FOR counter IN 1.._nIterations LOOP
        ST_LineInterpolatePoint(lp, 0.75)
        ELSE NULL
       END as p2_n,
-      p2 
+      lead_p 
       FROM (
        SELECT r.*, 
        CASE WHEN use_p1_n or use_p2_n THEN
-        ST_MakeLine(p1, p2)
+        ST_MakeLine(p1, lead_p)
         ELSE null 
        END as lp 
        FROM (
@@ -129,12 +124,7 @@ FOR counter IN 1.._nIterations LOOP
          true
          ELSE false
         END as use_p2_n
-        FROM (
-         SELECT  (dp).path[1] As org_index, (dp).geom As p1, lead((dp).geom) OVER () AS p2
-         FROM (
-          SELECT ST_DumpPoints(simplfied_line) as dp
-         ) as r 
-        ) as r
+   		FROM rdb_temp_table r
        ) as r
       ) as r
      ) as r
@@ -154,6 +144,7 @@ END LOOP;
 return simplfied_line;
   
 END; 
-$$ LANGUAGE plpgsql IMMUTABLE strict;
+$$ LANGUAGE plpgsql LEAKPROOF strict;
 
 select ST_AsText(chaikinSmoothLineComplex('0102000020E86400000300000000000000F89023410000000070FD584100000000F89023410000000075FD584100000000109123410000000075FD5841'));
+
